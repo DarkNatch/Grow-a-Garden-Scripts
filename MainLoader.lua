d997f4e60d5a813b98c6151e8f42e5ab5118a38c
DarkNatch <jeckonifuji@gmail.com>
2025-06-08 21:48:51 +0800

    MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -4,12 +4,12 @@ local FeatureConfig = {
 
     PetSpawner = {
         Enabled = true,
-        PetName = "Red Fox" -- Change to your pet name from RemoteSpy
+        PetName = "Ex.Red Fox" -- Change to your pet name from RemoteSpy
     },
 
     SeedSpawner = {
         Enabled = true,
-        SeedName = "Mango" -- Change to your seed name from RemoteSpy
+        SeedName = "Ex.Mango" -- Change to your seed name from RemoteSpy
     },
 
     EggDetector = true,
@@ -17,11 +17,11 @@ local FeatureConfig = {
 }
 
 local Features = {
-    LoadingScreen = "https://raw.githubusercontent.com/DarkNatch/GaG-Script/main/LoadingScreen.lua",
-    PetSpawner = "https://raw.githubusercontent.com/DarkNatch/GaG-Script/main/PetSpawner.lua",
-    SeedSpawner = "https://raw.githubusercontent.com/DarkNatch/GaG-Script/main/SeedSpawner.lua",
+    LoadingScreen = "https://github.com/DarkNatch/Grow-a-Garden-Scripts/blob/78e1208056fed2270a85917bedb7c064474dc131/LoadingScreen.lua",
+    PetSpawner = "https://github.com/DarkNatch/Grow-a-Garden-Scripts/blob/78e1208056fed2270a85917bedb7c064474dc131/ServerMangaer.lua",
+    SeedSpawner = "https://github.com/DarkNatch/Grow-a-Garden-Scripts/blob/78e1208056fed2270a85917bedb7c064474dc131/SeedSpawner.lua",
     EggDetector = "https://github.com/DarkNatch/Grow-a-Garden-Scripts/blob/108a8186f1ee9e61ae13629049d34a052b01c31f/EggDetector.lua",
-    ServerManager = "https://raw.githubusercontent.com/DarkNatch/GaG-Script/main/ServerManager.lua"
+    ServerManager = "https://github.com/DarkNatch/Grow-a-Garden-Scripts/blob/78e1208056fed2270a85917bedb7c064474dc131/ServerMangaer.lua"
 }
 
 local function loadFeature(name, url)
